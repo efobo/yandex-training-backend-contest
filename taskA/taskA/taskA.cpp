316dc908de48012fd05b73bf2f26d6ae2964b4d7
efobo <81191405+efobo@users.noreply.github.com>
2022-06-13 18:29:26 +0300

    A. Андрей и кислота

---
+++ taskA/taskA/taskA.cpp
@@ -0,0 +1,39 @@
+﻿// A. Андрей и кислота
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    vector<int> arr;
+    cin >> n;
+
+    int prev_x;
+    cin >> prev_x;
+    arr.push_back(prev_x);
+
+    for (int i = 1; i < n; i++)
+    {
+        int x;
+        cin >> x;
+
+        if (prev_x > x)
+        {
+            cout << -1;
+            return 0;
+        }
+        
+        arr.push_back(x);
+        prev_x = x;
+    }
+
+    int ans = arr[n-1] - arr[0];
+
+    cout << ans;
+}
+
+
